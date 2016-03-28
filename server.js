af81e6ebb4eb3306a6b310a3b64894681a874eea
barrybbb <barryjob@163.com>
2016-03-28 11:28:53 +0800

    Added files via upload
    Fixing Socket.IO For 0.9

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-//var Respoke = require('respoke-admin');
 var https = require('https');
-var http = require('http');
 
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
-
+process.env.NODE_ENV === "development"
 // API Routes
 var router = express.Router();
 
+// temp taken
+var tokenId = "yangbaiping123456" 
 // Create a token (accessed at POST /api/tokens)
 router.route('/tokens')
 	
@@ -23,41 +25,12 @@ router.route('/tokens')
 		console.log('Create new token using the Respoke API');
 		
 		console.log('POST /tokens endpointId: ', endpointId);
-		/*
-		// Consider putting credentials like appId and appSecret into a configuration file
-		var respoke = new Respoke({
-			appId: process.env.APP_ID,
-			'App-Secret': process.env.APP_SECRET
-		});
-		
-		respoke.auth.endpoint({
-			endpointId: endpointId, // The endpointId is the username entered by your app's end user
-			roleId: process.env.ROLE_ID // The roleId is the Id of endpoint role you created for permissions
-		}, function (err, response) {
-			if (err) {
-				console.log('respoke.auth.endpoint', err);
-				err.status = 500;
-			}
 
-			if (!response || !response.tokenId) {
-				res.json(err);
-			} else {
-				res.json({
-					token: response.tokenId
-				});
-			}
-		});
-		*/
 		res.json({
 					token: tokenId
-			});
+				});
 	});
 
-router.route('/webhooks')
-	
-	.post(function(req, res) {
-		console.log(req);
-	});
 //Handle v1/session token
 router.route('/session-tokens')
 	
@@ -71,14 +44,26 @@ router.route('/session-tokens')
 					token: tokenId
 				});
 	});
+
+router.route('/webhooks')
+	
+	.post(function(req, res) {
+		console.log(req);
+	});
+
 // Register our routes
 app.use('/api', router);
 app.use('/v1', router);
 app.use(express.static(__dirname + '/app'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
-// Start the server
-http.createServer(app).listen(port);
+
+// Socket IO.
+io.on('connection', function(socket){
+   console.log('Socket Conn Test.');
+});
+
+server.listen(port);
 
 if(process.env.NODE_ENV === "development") {
 	var fs = require('fs');
